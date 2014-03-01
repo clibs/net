4f3a194670882c3669afc6cdcef3caf51776a141
Yazhong Liu <l900422@vip.qq.com>
2014-03-02 03:07:04 +0800

    add test

--- tests/simple.c
+++ tests/simple.c
@@ -14,24 +14,30 @@ static void
 read_cb(net_t * net, size_t read, char * buf) {
   printf("%s\n", buf);
   printf("%zu\n", read);
+  assert(read > 0);
 }
 
-int
-main(int argc, char ** argv) {
-
+static void
+test_socket() {
   ssl_init();
   tls_ctx * ctx = tls_ctx_new();
   net_t * net = net_new("imap.gmail.com", 993);
   net->read_cb = read_cb;
-
   net_set_tls(net, ctx);
   net_connect(net);
+}
 
-  /* without ssl */
-  net_t * net_ = net_new("imap.qq.com", 143);
-  net_->read_cb = read_cb;
-  net_connect(net_);
+static void
+test_tls() {
+  net_t * net = net_new("imap.qq.com", 143);
+  net->read_cb = read_cb;
+  net_connect(net);
+}
 
+int
+main(int argc, char ** argv) {
+  test_socket();
+  test_tls();
   uv_run(net->loop, UV_RUN_DEFAULT);
   return 0;
 }
